92e93059f8770d80d131e76b14250aad458d6666
Lara Hollik <lara.hollik@edu.campus02.at>
2021-05-29 17:06:09 +0200

    +Added CashRegisterTest class and wrote the basic structure for it.

---
+++ src/test/java/at/fhj/iit/CashRegisterTest.java
@@ -0,0 +1,84 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CashRegisterTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getArticleNr() {
+    }
+
+    @Test
+    void setArticleNr() {
+    }
+
+    @Test
+    void getDay() {
+    }
+
+    @Test
+    void setDay() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getSells() {
+    }
+
+    @Test
+    void setSells() {
+    }
+
+    @Test
+    void getDrink() {
+    }
+
+    @Test
+    void setDrink() {
+    }
+
+    @Test
+    void getSeller() {
+    }
+
+    @Test
+    void setSeller() {
+    }
+
+    @Test
+    void salesNonAlc() {
+    }
+
+    @Test
+    void salesMildAlc() {
+    }
+
+    @Test
+    void salesStrongAlc() {
+    }
+
+    @Test
+    void salesOnDay() {
+    }
+
+    @Test
+    void salesOfSeller() {
+    }
+
+    @Test
+    void salesOfPersonOnDay() {
+    }
+}
